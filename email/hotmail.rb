4a082397f739ffb999a85b24707763a3161b5b4c
iknowicouldalwaysturn <iknowicouldalwaysturn@gmail.com>
2013-11-16 00:20:16 -0500

    Minimal bit of work done Friday, November 15, 2013

---
+++ email/hotmail.rb
@@ -0,0 +1,53 @@
+require_relative '../lib/app'
+
+class Hotmail < Email
+end
+
+class HotmailReg < Hotmail
+  def what_happened
+    @browser.goto 'https://edit.yahoo.com/registration?.intl=us&.lang=en-US&new=1&.done=http%3A//mail.yahoo.com&.src=ym'
+    @browser.text.include? 'Account'
+
+    ## registration form
+    @browser.form( :id => 'regFormBody' ).wait_until_present
+    main_form = @browser.form( :id => 'regFormBody' )
+
+    ## personal info
+    main_form.text_field( :id => 'firstname' ).set( @generate.first_name )
+    main_form.text_field( :id => 'secondname' ).set( @generate.last_name )
+    main_form.text_field( :id => 'gender' ).set( @generate.gender.capitalize )
+    main_form.select_list( :id => 'mm' ).set( @generate.month )
+    main_form.select_list( :id => 'dd' ).set( @generate.day )
+    main_form.select_list( :id => 'yyy' ).set( @generate.year )
+    main_form.select_list( :id => 'postalcode' ).set( @generate.zipcode )
+    
+    ## id/password
+    
+    #choose whether domain is going to be @ymail.com or @rocketmail.com.
+    if rand( 1...3 ) == 1
+      domain = 'yahoo.com'
+    else
+      domain = 'ymail.com'
+    end    
+    main_form.select_list( :id => 'domain' ).set( domain )
+    main_form.text_field( :id => 'yahooid' ).set( @generate.username )    
+    
+    main_form.text_field( :id => 'password' ).set( @generate.password )
+    main_form.text_field( :id => 'passwordconfirm' ).set( @generate.password )
+
+    ## recovery
+    main_form.select_list( :id => 'secquestion' ).set( main_form.select_list( :id => 'secquestion' ).option[ rand( 1...8 ) ] )
+    main_form.text_field( :id => 'secquestionanswer' ).set( @generate.phrase_one )
+
+    main_form.select_list( :id => 'secquestion2' ).set( main_form.select_list( :id => 'secquestion2' ).option[ rand( 1...17 ) ] )
+    main_form.text_field( :id => 'secquestionanswer2' ).set( @generate.phrase_two )
+
+    ## captcha
+    main_form.image( :id => 'captchaV5ClassicCaptchaImg' ).wait_until_present
+    captcha_answer = solve_captcha_image( 'id', 'captcha*' )
+    main_form.text_field( :name => 'captchaAnswer' ).set( captcha_answer )
+    
+    ## submit
+    main_form.button( :type => 'submit' ).click
+  end
+end
