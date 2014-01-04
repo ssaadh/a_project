50770f902a3c26db26f136f8ee886e298c352ee5
iknowicouldalwaysturn <iknowicouldalwaysturn@gmail.com>
2014-01-04 09:04:53 -0500

    !This and last two commits were done yesterday, Friday, January 3, 2013.  For this commit, used what was learned from Yahoo creation with Gmail. Began refactoring code

--- emails/gmail.rb
+++ emails/gmail.rb
@@ -32,6 +32,7 @@ end
 class GmailReg < Gmail
   def go_to    
     probability = rand( 1..7 )
+    puts "goto #{probability}"
     case probability
       when 1..2 then
         # 2
@@ -64,14 +65,14 @@ class GmailReg < Gmail
   
     private
     
-    def create_an_account_link
-      @browser.link( text: 'Create an account' ).click
-      #@browser.link( text: '/Create an account/' ).click
-    end
+      def create_an_account_link
+        @browser.link( text: 'Create an account' ).click
+        #@browser.link( text: '/Create an account/' ).click
+      end
     
-    def create_an_account_button
-      @browser.link( text: 'Create an account' ).click
-    end
+      def create_an_account_button
+        @browser.link( text: 'Create an account' ).click
+      end
   
   
   public
@@ -82,60 +83,119 @@ class GmailReg < Gmail
     
     go_to
     
-    ## registration form
+    # registration form
     @browser.form.wait_until_present
     
-    ## create email [div]
-    @browser.text_field( id: 'FirstName' ).set @current_email.first_name
-    @browser.text_field( id: 'LastName' ).set @current_email.last_name
-    
-    @browser.text_field( id: 'GmailAddress' ).set @current_email.username
-    
-    @browser.text_field( id: 'Passwd' ).set @current_email.password
-    @browser.text_field( id: 'PasswdAgain' ).set @current_email.password
-    
-    
-    ## about you [div]
-    
-    date_of_birth_specifics = DateOfBirthSpecifics.new @current_email.date_of_birth
-    
-    birth_month_element = @browser.element( id: 'BirthMonth' )
-    birth_month_element.click
-    birth_month_element.div( text: date_of_birth_specifics.month.capitalize ).click
-    @browser.text_field( id: 'BirthDay' ).set date_of_birth_specifics.day
-    @browser.text_field( id: 'BirthYear' ).set date_of_birth_specifics.birth_yyyy
-    
-    gender_element = @browser.element( id: 'Gender' )
-    gender_element.click
-    gender_element.div( text: @current_email.gender.capitalize ).click
-    
-    
-    ## recovery    
-    #@browser.text_field( id: 'RecoveryPhoneNumber' ).set @generate.mobile_number    
-    alternate_email_field( :id, 'RecoveryEmailAddress' )
+    ## main automation part
+    create_email_div_section
 
-    # No option for security question anymore
-    #recovery_question_list = @browser.select_list( id: 'acctSecurityQuestion' )
-    #recovery_question_list.set recovery_question_list.option[ rand( 1...6 ) ]
-    #@browser.text_field( id: 'acctSecurityAnswer' ).set #@generate.phrase_one
+    about_you_div_section
     
+    recovery_section
     
-    ## verify [div]
+    verify_div_section
     
-    ## captcha
-    @browser.image( src: /google\.com\/recaptcha/ ).wait_until_present
-    #captcha_answer = solve_captcha_image( 'src', 'regImageCaptcha' )
-    #@browser.text_field( :name => 'recaptcha_response_field' ).set( captcha_answer )
-    @browser.text_field( name: 'recaptcha_response_field' ).click
-    Watir::Wait.until( 60 ) { text_file_touched? }
-    remove_touched_file
-    
-    @browser.checkbox( id: 'TermsOfService' ).set
-    
-    ## submit
-    #@browser.button( id: 'submitbutton' ).click
-    @browser.send_keys :enter
+    terms_of_service_checkbox.set    
+    submit
   end
+  
+  def create_email_div_section
+    first_name_field.set @current_email.first_name
+    last_name_field.set @current_email.last_name
+    
+    username_field.set @current_email.username
+    
+    password_field.set @current_email.password
+    password_verification_field.set @current_email.password
+  end
+  
+  def about_you_div_section
+    birthday_month_selection( @current_email.birthday_specifics.month.capitalize )
+    birthday_day_field.set @current_email.birthday_specifics.day
+    birthday_year_field.set @current_email.birthday_specifics.year
+    
+    gender_selection( @current_email.gender.capitalize )
+  end
+  
+  def recovery_section
+    alternate_email_field.set @current_email.alternate_email.full
+  end
+  
+  def verify_div_section
+    ## captcha
+    captcha_image
+    #captcha_solving
+    captcha_response_field
+  end
+  
+  private
+  
+    def first_name_field
+      @browser.text_field( id: 'FirstName' )
+    end
+  
+    def last_name_field
+      @browser.text_field( id: 'LastName' )
+    end
+  
+    def username_field
+      @browser.text_field( id: 'GmailAddress' )
+    end
+  
+    def password_field
+      @browser.text_field( id: 'Passwd' )
+    end
+  
+    def password_verification_field
+      @browser.text_field( id: 'PasswdAgain' )
+    end
+    
+    def birthday_month_selection( month )
+      birth_month_element = @browser.element( id: 'BirthMonth' )
+      birth_month_element.click
+      birth_month_element.div( text: month ).click
+    end
+    
+    def birthday_day_field
+      @browser.text_field( id: 'BirthDay' )
+    end
+    
+    def birthday_year_field
+      @browser.text_field( id: 'BirthYear' )
+    end
+  
+    def gender_selection( gender )
+      gender_element = @browser.element( id: 'Gender' )
+      gender_element.click
+      gender_element.div( text: gender ).click
+    end
+    
+    def alternate_email_field
+      @browser.text_field( :id, 'RecoveryEmailAddress' )
+    end
+    
+    def captcha_image
+      @browser.image( src: /google\.com\/recaptcha/ ).wait_until_present
+    end
+    
+    def captcha_solving
+      @captcha_answer = solve_captcha_image( 'src', 'regImageCaptcha' )
+    end
+    
+    def captcha_response_field
+      @browser.text_field( name: 'recaptcha_response_field' ).click
+      Watir::Wait.until( 60 ) { text_file_touched? }
+      remove_touched_file
+    end
+    
+    def terms_of_service_checkbox
+      @browser.checkbox( id: 'TermsOfService' )
+    end
+    
+    def submit
+      #@browser.button( id: 'submitbutton' ).click
+      @browser.send_keys :enter        
+    end
   
   def confirm_alternate_email
     #@browser.text_field( id: 'password' ).set #@generate.password
